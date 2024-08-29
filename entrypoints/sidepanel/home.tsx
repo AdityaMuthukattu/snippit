e2bd987fe816c6ea28194b71ec0fdebfe1f3628f
Hellen Wang <hellenyilin23@gmail.com>
2024-08-30 05:39:50 +1000

    added scrollable area

--- entrypoints/sidepanel/home.tsx
+++ entrypoints/sidepanel/home.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import {Card} from "@/components/ui/card.tsx";
 import {useTranslation} from "react-i18next";
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 
 export function Home() {
     const {t} = useTranslation();
@@ -24,35 +26,26 @@ export function Home() {
         }
     ]
     return (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <Card className="text-left">
-                <div className="flex flex-col space-y-1.5 p-6 pb-3">
-                    <h3 className="font-semibold leading-none tracking-tight text-base">{t("introduce")}</h3>
+        <div className="flex flex-col min-h-screen">
+            <ScrollArea className="rounded-md border h-40 text-left">
+                <div className="flex flex-col space-y-4 p-6 pb-3">
+                    <h3 className="font-semibold leading-none tracking-tight text-base">{("introduce")}</h3>
                     <p className="text-sm max-w-lg text-balance leading-relaxed">
-                        {t("description")}
+                        {(" HTML code goes here fhdsfhdsfhodisfhodisfhoidfhdiosfh doisfhdoifhdsoifhdosif  fiodsjhfoidshfoidh fd dsfhdsoifhdsoif jdsoif hdsoifhd  fdiofh dosifjdosifj dsfd sfdsoifjdsoi jdoi fd oi fdjsoifdjofidjoi dso  df dsofd oi do fd fdjfds fjdf dsf df doif jdoifjdsoifjdsoi jdsof dsoi fdsof dsif dsof doisfjdsoif jdiosfjdsoi fdjfhdjksf fjdjfl ")}
                     </p>
                 </div>
-            </Card>
-            <Card className="text-left">
-                <div className="flex flex-col space-y-1.5 p-6 pb-3">
-                    <h3 className="font-semibold leading-none tracking-tight text-base">{t("reference")}</h3>
-                    <div className="flex flex-col gap-4 pt-2">
-                        {
-                            references.map((reference, index, array) => {
-                                return (
-                                    <div className="grid gap-1">
-                                        <p className="text-sm font-medium leading-none">
-                                            {reference.name}
-                                        </p>
-                                        <a className="text-sm text-muted-foreground" href={reference.url}
-                                           target="_blank">{reference.url}</a>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+            </ScrollArea>
+            <ScrollArea className="text-left">
+                <div className="flex flex-col space-y-4 p-6 pb-3">
+                    <h3 className="font-semibold leading-none tracking-tight text-base">{t("introduce")}</h3>
+                    <p className="text-sm max-w-lg leading-relaxed">
+                        {(" Hi, I'm Snippet, your personal guide to the world of websites!🎉 ")}
+                    </p>
+                    <p className="text-sm max-w-lg  leading-relaxed">
+                        {("I'm here to help you explore, understand, and even play with the building blocks of any website. To get started select a compnent on your screen")}
+                    </p>
                 </div>
-            </Card>
+            </ScrollArea>
         </div>
 
     )
