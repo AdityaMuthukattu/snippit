8dce83bbb1e4a7671ad82436618cf48f1faaff42
Hellen Wang <hellenyilin23@gmail.com>
2024-08-30 08:08:11 +1000

    added webpage

--- entrypoints/sidepanel/home.tsx
+++ entrypoints/sidepanel/home.tsx
@@ -3,28 +3,10 @@ import React from "react";
 import {Card} from "@/components/ui/card.tsx";
 import {useTranslation} from "react-i18next";
 import { ScrollArea } from "@/components/ui/scroll-area"
+import { Link } from "react-router-dom";
 
 
 export function Home() {
-    const {t} = useTranslation();
-    const references = [
-        {
-            name: "Wxt",
-            url: "https://wxt.dev/"
-        },
-        {
-            name: "React",
-            url: "https://react.dev/"
-        },
-        {
-            name: "Tailwind css",
-            url: "https://tailwindcss.com/"
-        },
-        {
-            name: "Shadcn Ui",
-            url: "https://ui.shadcn.com/"
-        }
-    ]
     return (
         <div className="flex flex-col min-h-screen">
             <ScrollArea className="rounded-md border h-40 text-left">
@@ -37,12 +19,15 @@ export function Home() {
             </ScrollArea>
             <ScrollArea className="text-left">
                 <div className="flex flex-col space-y-4 p-6 pb-3">
-                    <h3 className="font-semibold leading-none tracking-tight text-base">{t("introduce")}</h3>
+                    <h3 className="font-semibold leading-none tracking-tight text-base">{("introduce")}</h3>
                     <p className="text-sm max-w-lg leading-relaxed">
                         {(" Hi, I'm Snippet, your personal guide to the world of websites!🎉 ")}
                     </p>
                     <p className="text-sm max-w-lg  leading-relaxed">
-                        {("I'm here to help you explore, understand, and even play with the building blocks of any website. To get started select a compnent on your screen")}
+                        I'm here to help you explore, understand, and even play with the building blocks of any website. To get started select a component on your screen or click 
+                        <a href="/introduction" className="text-blue-500 underline">
+                            here
+                        </a> for our introductory docs
                     </p>
                 </div>
             </ScrollArea>
