136285adf0f30485281db1c9fa170527421ad666
Monash Sapkota <monash.sapkota@gmail.com>
2024-08-30 05:52:25 +1000

    add scrollable + viewport

--- entrypoints/sidepanel/home.tsx
+++ entrypoints/sidepanel/home.tsx
@@ -9,13 +9,33 @@ import { Separator } from "@/components/ui/separator";
 export function Home() {
   return (
     <div className="flex flex-col gap-2 min-h-screen">
-      <Card className="text-left">
-        <div className="space-y-1.5 p-6 pb-3 min-h-40">
+      <ScrollArea className="rounded-md border h-40 text-left">
+        <div className="flex flex-col space-y-4 p-6 pb-3">
+          <h3 className="font-semibold leading-none tracking-tight text-base">
+            {"introduce"}
+          </h3>
           <p className="text-sm max-w-lg text-balance leading-relaxed">
-            * viewport goes here *
+            {"HTML CODE GOES HERE"}
           </p>
         </div>
-      </Card>
+      </ScrollArea>
+      <ScrollArea className="text-left">
+        <div className="flex flex-col space-y-4 p-6 pb-3">
+          <h3 className="font-semibold leading-none tracking-tight text-base">
+            {"introduce"}
+          </h3>
+          <p className="text-sm max-w-lg leading-relaxed">
+            {
+              " Hi, I'm Snippet, your personal guide to the world of websites!🎉 "
+            }
+          </p>
+          <p className="text-sm max-w-lg  leading-relaxed">
+            {
+              "I'm here to help you explore, understand, and even play with the building blocks of any website. To get started select a compnent on your screen"
+            }
+          </p>
+        </div>
+      </ScrollArea>
       <div className="mt-auto w-full px-4 pb-20">
         <div className="flex flex-row items-center space-x-2 w-full ">
           <Input
